1a56bcd226f983ab9d9ceaf63ddff341921371e3
jack-davison <davison.jack.jd@gmail.com>
2025-12-12 11:24:12 +0000

    feat: remove old API endpoints

--- R/download_info.R
+++ R/download_info.R
@@ -4,32 +4,6 @@
 #' but provide assorted information about the EEA AQ database.
 #'
 #' @author Jack Davison
-#'
-#' @rdname api-info
-#' @export
-get_eea_ddb_extension_info <- function() {
-  httr2::request(
-    "https://eeadmz1-downloads-api-appservice.azurewebsites.net/DDBExtensionInfo"
-  ) |>
-    httr2::req_method("POST") |>
-    httr2::req_perform() |>
-    httr2::resp_body_json(simplifyVector = TRUE) |>
-    dplyr::tibble()
-}
-
-#' @rdname api-info
-#' @export
-get_eea_ddb_vars <- function() {
-  httr2::request(
-    "https://eeadmz1-downloads-api-appservice.azurewebsites.net/DDBVars"
-  ) |>
-    httr2::req_method("POST") |>
-    httr2::req_perform() |>
-    httr2::resp_body_json(simplifyVector = TRUE) |>
-    dplyr::tibble()
-}
-
-#' @rdname api-info
 #' @export
 get_eea_version <- function() {
   httr2::request(
